83f2ebeac5010bcebfa4a9f5c7e97a987bcc8931
rob <student@epicodus.com>
2016-03-04 16:56:10 -0800

    add fixed header

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 //business logic
 
 //interface
-// $(document).ready(function() {
-// });
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-navigation-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-navigation-menu').slideToggle(function(){
+      if($('#js-navigation-menu').is(':hidden')) {
+        $('#js-navigation-menu').removeAttr('style');
+      }
+    });
+  });
+});
