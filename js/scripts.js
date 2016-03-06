fd59a328f5c339cddf28a29cb471e86c46967490
RobinBKing <samimspop@gmail.com>
2016-03-05 21:32:13 -0800

    add all header compontents including bottom nav

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,19 @@
 //interface
 $(document).ready(function() {
   var menuToggle = $('#js-mobile-menu').unbind();
-  $('#js-navigation-menu').removeClass("show");
+  $('#js-main-menu').removeClass("show");
+  // $('#js-category-menu').removeClass("show");
 
   menuToggle.on('click', function(e) {
     e.preventDefault();
-    $('#js-navigation-menu').slideToggle(function(){
-      if($('#js-navigation-menu').is(':hidden')) {
-        $('#js-navigation-menu').removeAttr('style');
+    $('#js-main-menu').slideToggle(function(){
+      if($('#js-main-menu').is(':hidden')) {
+        $('#js-main-menu').removeAttr('style');
       }
+      // $('#js-category-menu').slideToggle(function(){
+      //   if($('#js-category-menu').is(':hidden')) {
+      //     $('#js-category-menu').removeAttr('style');
+      //   }
     });
   });
 });
